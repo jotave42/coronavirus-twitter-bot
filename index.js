4fe4471a5a7f50f2ae55182cc5d97f82b60191f6
jotave42 <jvictorcostamelo@gmail.com>
2020-03-05 13:02:10 -0300

    log has improved

--- index.js
+++ index.js
@@ -4,35 +4,49 @@ const fetch = require("node-fetch");
 const Twit = require("twit");
 const tokens  = require("./tokens.json");
 
-const Tweet =  (bot,jsonFile) =>{
-    const tweet = `Coronavirus Update \n`
-                + `Country_Region: ${jsonFile.Country_Region}\n`
-                + `Confirmed: ${jsonFile.Confirmed}\n`
-                + `Deaths: ${jsonFile.Deaths}\n`
-                + `Recovered: ${jsonFile.Recovered}\n`
-                + `The data comes from: tinyurl.com/uwns6z5\n`
-                + `#Coronavirus #COVID19 #bot`  
+const Tweet =  (bot,jsonFile,oldJson) =>{
+    let tweet;
+    if(!oldJson){
+        tweet = `Coronavirus Update \n`
+                    + `Country_Region: ${jsonFile.Country_Region}\n`
+                    + `Confirmed: ${jsonFile.Confirmed}\n`
+                    + `Deaths: ${jsonFile.Deaths}\n`
+                    + `Recovered: ${jsonFile.Recovered}\n`
+                    + `The data comes from: tinyurl.com/uwns6z5\n`
+                    + `#Coronavirus #COVID19 #bot`  
+    } else {
+        tweet = `Coronavirus Update \n`
+                    + `Country_Region: ${jsonFile.Country_Region}\n`
+                    + `Confirmed: ${oldJson.Confirmed} => ${jsonFile.Confirmed}\n`
+                    + `Deaths: ${oldJson.Deaths} => ${jsonFile.Deaths}\n`
+                    + `Recovered: ${oldJson.Recovered} => ${jsonFile.Recovered}\n`
+                    + `The data comes from: tinyurl.com/uwns6z5\n`
+                    + `#Coronavirus #COVID19 #bot`  
+    }
     bot.post('statuses/update', { status: tweet }, (err, data, response) => {
-            if(!err){
-                console.log("Tweet success");
+        const today = datetime.toLocaleString("pt-BR"); 
+        if(!err){
+                console.log(`[${today}] Tweet success`);
             } else {
                 console.log(err.message);
                 if(err.message ==="Status is a duplicate."){
-                    console.log("ignored");
+                    console.log(`[${today}] ignored`);
                     return;
                 }
-                console.log("Tweet faild trying again");
+                console.log(`[${today}] Tweet faild trying again`);
                 Tweet(bot,jsonFile);
             }
       });
 };
 
-const saveFile =  (jsonFile, fileName) => {
+const saveFile =  (jsonFile, fileName,today) => {
     fs.writeFileSync(fileName,JSON.stringify(jsonFile,null,4));
-    console.log(`File ${fileName} saved.`);
+    console.log(`[${today}] File ${fileName} saved.`);
 };
 
 const downloadFiles  = async () =>{
+    const datetime = new Date();
+    let today = datetime.toLocaleString("pt-BR"); 
     console.log("Starting Download..")
     const bot = new Twit({
         consumer_key: tokens.APIKey,
@@ -64,15 +78,15 @@ const downloadFiles  = async () =>{
                 const rawdata = fs.readFileSync(fileName);
                 const oldJson = JSON.parse(rawdata);
                 if( (newJson.Confirmed == oldJson.Confirmed) && (newJson.Deaths == oldJson.Deaths) && (newJson.Recovered == oldJson.Recovered) ){
-                    console.log("Nothing change at: ",newJson.Country_Region);
+                    console.log(`[${today}] Nothing change at: ${newJson.Country_Region}`);
                 } else {
-                    console.log("Change at: ",newJson.Country_Region);
-                    saveFile(newJson, fileName);
-                    Tweet(bot,newJson);
+                    console.log(`[${today}] Change at: ${newJson.Country_Region}`);
+                    saveFile(newJson, fileName,today);
+                    Tweet(bot,newJson,oldJson);
 
                 }
             } else {
-                saveFile(newJson, fileName);
+                saveFile(newJson, fileName,today);
                 Tweet(bot,newJson);
             }
          
